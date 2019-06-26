83b87f8fbcc25da0da0285161f9f04d3db3952ae
iris1114 <qweichew@gmail.com>
2019-06-26 12:09:46 +0800

    iris-update

--- iris/gulpfile.js
+++ iris/gulpfile.js
@@ -24,7 +24,7 @@ function browserSync(done) {
     browsersync.init({
         server: {
             baseDir: "./",
-            index: "common.html"
+            index: "index.html"
         },
         port: 3000
     });
