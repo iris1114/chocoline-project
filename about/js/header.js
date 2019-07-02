7814bb572ea6077d62316c7f837e7f5be4e479e8
owlnightfall <51066347+owlnightfall@users.noreply.github.com>
2019-07-02 19:39:22 +0800

    about
    about update

---
+++ about/js/header.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function() {
+  let burger = document.querySelector(".burger figure");
+  let menubox = document.querySelector(".menubox");
+  let menuclose = document.querySelector("#menuclose");
+
+  burger.addEventListener("click", function() {
+    menubox.style.display = "block";
+    setTimeout(function() {
+      menubox.classList.add("menuboxopen");
+    }, 100);
+    document.body.style.overflowY = "hidden";
+  });
+
+  menuclose.addEventListener("click", function() {
+    menubox.classList.remove("menuboxopen");
+    setTimeout(function() {
+      menubox.style.display = "none";
+    }, 500);
+    document.body.style.overflowY = "auto";
+  });
+});
