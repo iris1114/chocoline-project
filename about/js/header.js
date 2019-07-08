6c628e9d19831ee15e2be0a8aa33dbd92059f083
owlnightfall <51066347+owlnightfall@users.noreply.github.com>
2019-07-08 18:34:09 +0800

    penny0708-3

--- about/js/header.js
+++ about/js/header.js
@@ -1,21 +1,22 @@
-window.addEventListener("load", function() {
-  let burger = document.querySelector(".burger figure");
-  let menubox = document.querySelector(".menubox");
-  let menuclose = document.querySelector("#menuclose");
+window.addEventListener("load",function(){
+    let burger = document.querySelector(".burger figure");
+    let menubox = document.querySelector(".menubox");
+    let menuclose = document.querySelector("#menuclose");
 
-  burger.addEventListener("click", function() {
-    menubox.style.display = "block";
-    setTimeout(function() {
-      menubox.classList.add("menuboxopen");
-    }, 100);
-    document.body.style.overflowY = "hidden";
-  });
+    burger.addEventListener("click",function(){
+        menubox.style.display="block";
+        setTimeout(function(){
+            menubox.classList.add("menuboxopen");
+        },100)
+        document.body.style.overflowY = "hidden";
+    });
 
-  menuclose.addEventListener("click", function() {
-    menubox.classList.remove("menuboxopen");
-    setTimeout(function() {
-      menubox.style.display = "none";
-    }, 500);
-    document.body.style.overflowY = "auto";
-  });
-});
+    menuclose.addEventListener("click",function(){
+        
+        menubox.classList.remove("menuboxopen");
+        setTimeout(function(){
+            menubox.style.display="none";
+        },500)
+        document.body.style.overflowY = "auto";
+    })
+})
