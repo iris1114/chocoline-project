67fce952f3e1e35b5f9e8e5fedd214b70b31fc72
owlnightfall <51066347+owlnightfall@users.noreply.github.com>
2019-07-18 18:01:00 +0800

    update0718

--- about/about.php
+++ about/about.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if(!isset($_SESSION["mem_id"])){
+    $_SESSION["mem_id"] = null;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,6 +16,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>關於 CHOCO</title>
     <link rel="stylesheet" href="css/about.css" />
+    <link rel="stylesheet" href="css/about.css" />
     <link
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
@@ -19,72 +27,143 @@
   </head>
   <body>
     <!-- header start -->
-    <header>
-      <div class="m_header">
+  <header>
+    <div class="m_header">
         <div class="navbar">
-          <div class="burger">
-            <figure>
-              <img src="image/headerfooter/burger.png" alt="burger" />
-            </figure>
-          </div>
-          <div class="logo">
-            <a href="../index/index.php">
-              <img src="image/headerfooter/logo.png" alt="CHOCOLINE" />
-            </a>
-          </div>
-          <div class="status">
-            <figure>
-              <a href="../member/member.php">
-                <img src="image/headerfooter/icon_member.png" alt="member" />
-              </a>
-            </figure>
-            <figure>
-              <a href="../cart/cart.php">
-                <img src="image/headerfooter/icon_cart.png" alt="cart" />
-              </a>
-            </figure>
-          </div>
+            <div class="burger">
+                <figure>
+                    <img src="../common/image/headerfooter/burger.png" alt="burger">
+                </figure>
+            </div>
+            <div class="logo">
+                <h1>
+                    CHOCOLINE
+                </h1>
+                <a href="../index/index.php">
+                    <img src="../common/image/headerfooter/logo.png" alt="CHOCOLINE">
+                </a>
+            </div> 
+            <div class="status">
+                <figure>
+                    <a class="spanLogin" href="javascript:;">
+                        <img src="../common/image/headerfooter/icon_member.png" alt="member" />
+                        <!-- icon點擊後跳出登入註冊燈箱 -->
+                        <span id="mem_id_hide_mobile" style="display:none"><?php echo $_SESSION["mem_id"]?></span>
+                        <span id="spanLoginText_mobile" style="display:none">登入</span>
+                    </a>
+                </figure>
+                <figure>
+                    <a href="../cart/cart.php">
+                        <img src="../common/image/headerfooter/icon_cart.png" alt="cart" />
+                    </a>
+                </figure>
+            </div>
         </div>
         <ul class="menubox">
-          <li><a href="../custom/custom.php">客製 CHOCO</a></li>
-          <li><a href="../contest/contest.php">CHOCO 選美</a></li>
-          <li><a href="../game/game.php">CHOCO 遊戲</a></li>
-          <li><a href="../store/store.php">CHOCO 商城</a></li>
-          <li><a href="../about/about.php">關於 CHOCO</a></li>
-          <figure id="menuclose">
-            <img src="image/headerfooter/menuclose.png" alt="close" />
-          </figure>
-        </ul>
-      </div>
-      <div class="d_header">
-        <div class="logo">
-          <a href="../index/index.php">
-            <img src="image/headerfooter/logo.png" alt="CHOCOLINE" />
-          </a>
-        </div>
-        <div class="navbar">
-          <ul class="menubox">
             <li><a href="../custom/custom.php">客製 CHOCO</a></li>
             <li><a href="../contest/contest.php">CHOCO 選美</a></li>
             <li><a href="../game/game.php">CHOCO 遊戲</a></li>
             <li><a href="../store/store.php">CHOCO 商城</a></li>
-            <li class="nowpage"><a href="../about/about.php">關於 CHOCO</a></li>
-          </ul>
-          <div class="status">
-            <figure>
-              <a href="../member/member.php">
-                <img src="image/headerfooter/icon_member.png" alt="member" />
-              </a>
+            <li><a href="../about/about.php">關於 CHOCO</a></li>
+            <figure id="menuclose">
+                <img src="../common/image/headerfooter/menuclose.png" alt="close">
             </figure>
-            <figure>
-              <a href="../cart/cart.php">
-                <img src="image/headerfooter/icon_cart.png" alt="cart" />
-              </a>
-            </figure>
-          </div>
+        </ul>
+    </div>
+    <div class="d_header">
+        <div class="logo">
+            <h1>
+                CHOCOLINE
+            </h1>
+            <a href="../index/index.php">
+                <img src="../common/image/headerfooter/logo.png" alt="CHOCOLINE">
+            </a>
         </div>
-      </div>
-    </header>
+        <div class="navbar">
+            <ul class="menubox">
+                <li><a href="../custom/custom.php">客製 CHOCO</a></li>
+                <li><a href="../contest/contest.php">CHOCO 選美</a></li>
+                <li><a href="../game/game.php">CHOCO 遊戲</a></li>
+                <li><a href="../store/store.php">CHOCO 商城</a></li>
+                <li class="nowpage"><a href="../about/about.php">關於 CHOCO</a></li>
+            </ul>
+            <div class="status">
+            <figure>
+                <a class="spanLogin" href="javascript:;">
+                    <img src="../common/image/headerfooter/icon_member.png" alt="member" />
+                    <!-- icon點擊後跳出登入註冊燈箱 -->
+                </a>
+                <span id="mem_id_hide" style="display:none"><?php echo $_SESSION["mem_id"]?></span>
+                <span id="spanLoginText" style="display:none">登入</span>
+            </figure>
+            <figure>
+                <a href="../cart/cart.php">
+                    <img src="../common/image/headerfooter/icon_cart.png" alt="cart" />
+                </a>
+            </figure>
+            </div>
+        </div>
+    </div>
+    <!-- 燈箱：登入 -->
+    <div id="lightBox" style="display:none">
+        <div id="tableLogin">
+            <img class="login_bg" src="../common/image/login/login_bg.png" alt="login_bg">
+            <div class="login_password">
+                <a href="javascript:;" class="btnLoginCancel">
+                    <img src="../common/image/login/login_closeicon.png" alt="">
+                </a>			
+                <h3>會員登入</h3>
+                <input type="text" name="mem_id" id="mem_id" value="" placeholder="帳號"><br>
+                <input type="password" name="mem_psw" id="mem_psw" value="" placeholder="密碼"><br>
+                <a href="javascript:;" id="forget_password">忘記密碼</a><br>
+                <a href="javascript:;" class="btn orange_l" id="btnLogin">登入</a><br>
+                <span>不是會員嗎?</span>
+                <a href="javascript:;" id="register">立即註冊</a><br>
+            </div>
+        </div>
+    </div>
+    <!-- 重設密碼 -->
+    <div id="passwordLightBox" style="display:none">
+        <div id="getPassword">
+            <img class="login_bg" src="image/login/login_bg.png" alt="login_bg">
+            <div class="login_password">
+                <a href="javascript:;" class="btnLoginCancel">
+                    <img src="image/login/login_closeicon.png" alt="">
+                </a>			
+                <a href="javascript:;" id="rebtnLogin">會員登入</a><br>
+                <h3>重設密碼</h3>
+                <p>請輸入帳號註冊時所留的電子<br>
+                    郵件地址，以驗證您的資料</p>
+                <input type="email" name="mem_email" id="mem_email" value="" placeholder="輸入E-mail"><br>
+                <input type="password" name="mem_psw" id="new_mem_psw" value="" placeholder="輸入新密碼  (6-12位字母、數字)"><br>
+                <input type="password" name="mem_psw" id="re_new_mem_psw" value="" placeholder="再次確認新密碼"><br>
+                <a href="javascript:;" class="btn orange_l" id="repassword">送出</a><br>
+            </div>
+        </div>
+    </div>
+    <!-- 會員註冊 -->
+    <div id="registerLightBox" style="display:none">
+        <div id="registered">
+            <img class="login_bg" src="image/login/login_bg.png" alt="login_bg">
+            <div class="login_register">
+                <a href="javascript:;" class="btnLoginCancel">
+                    <img src="image/login/login_closeicon.png" alt="btnLoginCancel">
+                </a>			
+                <h3>會員註冊</h3>
+                <p>嗨！新朋友～歡迎加入CHOCOLINE會員<br>
+                        請填下您的個人資料！* 為必填。</p>
+                <span>*帳號</span><input type="text" name="mem_id" id="f_mem_id" value="" placeholder="設定帳號"><br>
+                <span><input type="button" id="btnCheckId" value="檢查帳號是否可用"></span>
+                <p id="idMsg">請輸入帳號</p><br>
+                <span>*E-mail</span><input type="email" name="mem_email" id="f_mem_email" value="" placeholder="輸入E-mail 必須包括 ( @ 和 . )" ><br>
+                <span>*密碼</span><input type="password" name="mem_psw" id="f_mem_psw" value="" placeholder="設定密碼 (6-12位字母、數字)"><br>
+                <span>*密碼確認</span><input type="password" name="mem_psw" id="f_re_mem_psw" value="" placeholder="再次確認密碼 (再次確認)"><br>
+                <a href="javascript:;" class="btn orange_l" id="register_btn">送出</a><br>
+            </div>
+        </div>
+    </div>
+</header>
+
     <!-- header end -->
     <section class="container">
       <div class="aboutus">
@@ -380,6 +459,7 @@
     <script src="../common/js/robot.js"></script>
     <!-- robot新增js  -->
     <script src="../common/js/header.js"></script>
+    <script src="../common/js/login.js"></script>
     <script src="js/main.js"></script>
   </body>
 </html>
