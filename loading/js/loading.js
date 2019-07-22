6e6d3f30831bdc2d90a6c684a2f536800c018026
owlnightfall <51066347+owlnightfall@users.noreply.github.com>
2019-07-22 13:03:32 +0800

    loading 0722

---
+++ loading/js/loading.js
@@ -0,0 +1,28 @@
+
+
+// --------------loading---------------
+
+var btn = document.getElementById("front_end_img") //獲取btn元素
+console.log(btn);
+
+
+btn.onclick = function() { //設定當頁面載入時執行
+    $('#front_end_img').addClass('fly');
+    setTimeout(function() {
+        window.location.href = "../index/index.php";
+    }, 4000);    
+    $('')
+}
+
+
+var btn = document.getElementById("back_end_img") //獲取btn元素
+console.log(btn);
+
+
+btn.onclick = function() { //設定當頁面載入時執行
+    $('#back_end_img').addClass('fly2');
+    setTimeout(function() {
+        window.location.href = "../index/index.php";
+    }, 4000);    
+    $('')
+}
